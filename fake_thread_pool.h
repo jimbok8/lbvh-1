928cc093ba18439e47266fe943e94ec5fc97a7c2
Taylor Holberton <taylorcholberton@gmail.com>
2020-04-08 15:10:00 -0400

    Using current thread for work as well

--- fake_thread_pool.h
+++ fake_thread_pool.h
@@ -18,7 +18,7 @@ public:
   //! Constructs a new fake task scheduler.
   //! \param max_threads The maximum number of threads to run.
   fake_task_scheduler(size_type max_threads_)
-    : max_threads(max_threads_) { }
+    : max_threads(max_threads_ ? max_threads_ : 1) { }
   //! Schedules a new task to be completed.
   //! \tparam task_type The type of the task functor.
   //! \tparam arg_types The arguments to pass to the thread.
@@ -27,12 +27,14 @@ public:
 
     std::vector<std::thread> threads;
 
-    for (size_type i = 0; i < max_threads; i++) {
+    for (size_type i = 0; i < max_threads - 1; i++) {
 
       work_division div { i, max_threads };
 
       threads.emplace_back(task, div, args...);
     }
+
+    task(work_division { max_threads - 1, max_threads }, args...);
 
     for (auto& th : threads) {
       th.join();
